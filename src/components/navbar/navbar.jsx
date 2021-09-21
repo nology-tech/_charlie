8cfae7bd9d9a7f12c72e992819df1dceaf76531a
Reshmy Devaraju <reshmyd@gmail.com>
2021-09-21 16:33:55 +0100

    imported icons for the navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./navbar.scss";
+import { FaHome, FaFileAlt, FaUsers, FaIdBadge, FaCog } from 'react-icons/fa';
 
 const Navbar = () => {
+    
     const navItems = [
-        {link: "/", text: "Dashboard", icon: ""},
-        {link: "/students", text: "Students", icon: ""},
-        {link: "/projects", text: "Projects", icon: ""},
-        {link: "/enrollment", text: "Enrollment", icon: ""},
-        {link: "/settings", text: "Settings", icon: ""},
+        {link: "/", text: "Dashboard", icon: <FaHome /> },
+        {link: "/students", text: "Students", icon: <FaFileAlt />},
+        {link: "/projects", text: "Projects", icon: <FaUsers />},
+        {link: "/enrollment", text: "Enrollment", icon: <FaIdBadge />},
+        {link: "/settings", text: "Settings", icon: <FaCog />},
     ];
     
-    const x = navItems.map(({ link, text, icon }) => {
-        return (
-            <>
-                <Link to={link}>
-                    {text}
-                    <img src={icon} alt={text} />
+     const x = navItems.map(({ link, text, icon }) => {
+    return (
+           
+               <Link to={link}>
+                  {icon}
+                 {text}
+                    
                 </Link>
-            </>
+           
         )
-    });
+     });
     
     return (
         <div>
