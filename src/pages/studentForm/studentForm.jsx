2e06cd3dbb1d4b8513bbfccf9991817078783365
Noor Ncho <nrandin@gmail.com>
2021-09-22 16:01:45 +0100

    got general boostrap 2 column layout

--- src/pages/studentForm/studentForm.jsx
+++ src/pages/studentForm/studentForm.jsx
@@ -1,43 +1,60 @@
 import React from 'react';
 import studentData from '../../data/studentForm';
+import {useForm} from "react-hook-form";
 
 const StudentForm = () => {
+    const {register} = useForm();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const data = {}
-        formData.forEach((value, key) => (data[key] = value))
-        // Log the data.
-        console.log(JSON.stringify(data))
-        studentData.push(JSON.stringify(data))
+        formData.forEach((value, key) => (data[key] = value));
+        console.log(JSON.stringify(data));
+        studentData.push(JSON.stringify(data));
         console.log('this is the student data object:', studentData);
     }
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">First Name</label>
-                <input type="text" name="first-name" id="first-name" />
-
-                <label htmlFor="">Last Name</label>
-                <input type="text" name="last-name" id="last-name" />
-
-                <label htmlFor="">Enrolled On</label>
-                <select id="enrolled-course" name="enrolled-course">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="fiat">Fiat</option>
-                    <option value="audi">Audi</option>
-                </select>
-
-                <label htmlFor="">Github Account</label>
-                <input type="text" name="github-account" id="github-account" />
-                <label htmlFor="">Portfolio Link</label>
-                <input type="text" name="portfolio-link" id="portfolio-link" />
-
-                <input type="submit" value="Save" />
+        <div className="row text-center">
+            <form class="row" onSubmit={handleSubmit}>
+                <div className="col-6"> 
+                    <div className="col-md-12">                
+                        <label htmlFor="">First Name</label>
+                        <input type="text" {...register("firstName")} id="firstName" />
+                    </div>
+                    <div className="col-md-12">   
+                        <label htmlFor="">Last Name</label>
+                        <input type="text" {...register("lastName")} id="lastName" />
+                        </div>
+                    <div className="col-md-12"> 
+                        <label htmlFor="">Enrolled On</label>
+                        <select id="enrolled-course" {...register("enrolled-course")}>
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            <option value="fiat">Fiat</option>
+                            <option value="audi">Audi</option>
+                        </select>
+                        </div>
+                    <div className="col-md-12"> 
+                        <label htmlFor="">Github Account</label>
+                        <input type="text" {...register("github-account")} id="github-account" />
+                        </div>
+                    <div className="col-md-12"> 
+                        <label htmlFor="">Portfolio Link</label>
+                        <input type="text" {...register("portfolio-link")} id="portfolio-link" />
+                        </div>
+                </div>
+                <div className="col-6">
+                    <div className="col-md-12">                
+                        <label htmlFor="">First Name</label>
+                        <input type="text" {...register("firstName")} id="firstName" />
+                        <input type="submit" value="Save" />
+                    </div>  
+                </div>
             </form>
+        </div>
         </>
     )
 }
