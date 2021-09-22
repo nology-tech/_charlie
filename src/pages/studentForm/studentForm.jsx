a37d4ff245d3c6fcf5dce864c55f43b8e8db06d8
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-22 18:15:14 +0200

    test img

--- src/pages/studentForm/studentForm.jsx
+++ src/pages/studentForm/studentForm.jsx
@@ -15,16 +15,30 @@ const StudentForm = () => {
         console.log('this is the student data object:', studentData);
     }
 
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                ('#blah')
+                    .attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
     return (
         <>
         <div className="row text-center">
-            <form class="row" onSubmit={handleSubmit}>
-                <div className="col-6"> 
-                    <div className="col-md-12">                
+            <form className="row" onSubmit={handleSubmit}>
+  {/*               <div className="col-6"> 
+                    <div className="col-md-10">                
                         <label htmlFor="">First Name</label>
-                        <input type="text" {...register("firstName")} id="firstName" />
+                        <input className="form-control" type="text" {...register("firstName")} id="firstName" />
                     </div>
-                    <div className="col-md-12">   
+                    <div className="col-md-12">
                         <label htmlFor="">Last Name</label>
                         <input type="text" {...register("lastName")} id="lastName" />
                         </div>
@@ -48,11 +62,14 @@ const StudentForm = () => {
                 </div>
                 <div className="col-6">
                     <div className="col-md-12">                
-                        <label htmlFor="">First Name</label>
-                        <input type="text" {...register("firstName")} id="firstName" />
-                        <input type="submit" value="Save" />
+                    <span class="btn btn-file">Upload<input label="upload" type="file" /></span>
                     </div>  
                 </div>
+                <div className="col-12">
+                    <input type="submit" value="Save" /> */}
+                    <input type='file' onchange={readURL} />
+                <img id="blah" src="http://placehold.it/180" alt="your image" />
+              {/*   </div> */}
             </form>
         </div>
         </>
