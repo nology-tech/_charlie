f336f521ccd3b1d0d1efc3a18173e0f366473ea4
Reshmy Devaraju <reshmyd@gmail.com>
2021-09-23 16:15:10 +0100

    added more test to the testing file

--- src/App.test.js
+++ src/App.test.js
@@ -16,3 +16,21 @@ it("Should render the navbar", () => {
     const logo = screen.getByAltText('logo');
     expect(logo.src).toContain("/Logo.png");
 });
+
+it("On clicking the links they go the respective pages", () => {
+  render(<App />);
+  const pageRegexes = [
+    [/dashboard/i, "/" ], 
+    [/students/i, "/students"], 
+    [/projects/i, "/projects"], 
+    [/enrollment/i, "/enrollment"], 
+    [/settings/i, "/settings"]
+  ];
+  pageRegexes.forEach(regex => {
+    const navLink = screen.getByRole("link", { name: regex[0] });
+    userEvent.click(navLink)
+    expect(global.window.location.pathname).toContain(regex[1]);
+  })
+})
+
+
