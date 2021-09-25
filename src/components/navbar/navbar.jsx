db448e2e7548830ed928c71305c624599a07cf51
saagarshah99 <saagarshahs@hotmail.com>
2021-09-25 22:39:17 +0100

    Fixed: active navlink for current page in navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
-
 import "./navbar.scss";
 
 import Logo from "../../assets/images/Logo.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { FaHome, FaFileAlt, FaUsers, FaIdBadge, FaCog } from 'react-icons/fa';
 
 const Navbar = () => {
-    const [activePage, setActivePage] = useState("/");
-    const updateActive = (event) => {
-        setActivePage(global.window.location.pathname);
-        event.target.className += " active";
-
-        
-    }
-    
     const navItems = [
         {link: "/", text: "Dashboard", icon: <FaHome /> },
         {link: "/students", text: "Students", icon: <FaFileAlt />},
@@ -22,23 +12,24 @@ const Navbar = () => {
         {link: "/enrollment", text: "Enrollment", icon: <FaIdBadge />}
     ];
     
+    // using Navlink in order to add an active class for the current page
     const mappedNavItems = navItems.map(({ link, text, icon }, index) => {
         return (
-            <Link to={link} onClick={updateActive} className="navbar__link" key={index}>
+            <NavLink exact to={link} activeClassName="active" className="navbar__link" key={index}>
                 <span className="navbar__link-icon">
                     {icon}
                 </span>
                 <span className="navbar__link-text">
                     {text}
                 </span>
-            </Link>
+            </NavLink>
 
         )
     });
     
     return (
         <div className="navbar" >
-            <Link to="/" className="navbar__link" >
+            <Link to="/" className="navbar__link-logo" >
                 <img src={Logo} alt="logo"  />
             </Link>
             <hr />
@@ -46,14 +37,14 @@ const Navbar = () => {
             {mappedNavItems}
 
             <hr />
-            <Link to="/settings" onClick={updateActive} className="navbar__settings navbar__link" >
+            <NavLink exact to="/settings" activeClassName="active" className="navbar__settings navbar__link" >
                 <span className="navbar__link-icon">
                     <FaCog />
                 </span>
                 <span className="navbar__link-text">
                     Settings
                 </span>
-            </Link>
+            </NavLink>
         </div>  
     );
 }
