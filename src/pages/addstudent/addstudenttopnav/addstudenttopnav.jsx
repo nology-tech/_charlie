6420ce71fbed02171b61878221791621e1ba193b
Noor Ncho <nrandin@gmail.com>
2021-09-27 16:51:40 +0100

    added stlying to topnav

--- src/pages/addstudent/addstudenttopnav/addstudenttopnav.jsx
+++ src/pages/addstudent/addstudenttopnav/addstudenttopnav.jsx
@@ -3,11 +3,11 @@ import "./addstudenttopnav.scss";
 
 const AddStudentTopNav = () => {
     return (
-            <div className="row top-nav d-flex align-items-center">
-                <h1 className="col-3 offset-1">Add Student</h1>
+            <div className="row topnav d-flex align-items-center">
+                <h1 className="col-3 offset-1 topnav__title">Add Student</h1>
                 <div className="top-nav__buttons col-5 offset-3">
-                    <button className="col-3 btn btn-secondary"> Go Back</button>
-                    <button className="col-3 btn btn-primary">Save</button>
+                    <button className="col-3 btn btn-secondary topnav__button mx-2"> Go Back</button>
+                    <button className="col-3 btn btn-primary topnav__button">Save</button>
                 </div>
             </div>
     )
