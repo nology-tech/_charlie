ce8b7d508363016ff79aa5ef9f67302935955222
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-28 15:38:14 +0200

    adding test samples in order to start testing

---
+++ src/pages/addstudent/__tests__/addstudent.test.js
@@ -0,0 +1,11 @@
+// Positive (valid) and negative tests (invalid)
+
+it ('Should redirect to the student list when given valid inputs', () => {
+
+})
+
+it ('should show error messages when a student name is not provided', () => {
+  
+})
+
+
