3cc669a47a39574290f388a190758b89d4280af8
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-30 15:51:57 +0200

    Linked form to studentlist create button

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import { useHistory } from 'react-router';
-
-//The react-router property useHistory is applied to make it easier to got back to the last page/url
 
 const Dashboard = () => {
-    const history = useHistory();
     return (
         <div>
-            <button onClick={() => history.push('/student-details')}>Add Student</button>
+
         </div>
     )
 }
