cf20cb6cd83403aa8491079ded29787e85e6f6e3
Noor Ncho <nrandin@gmail.com>
2021-09-30 14:43:14 +0100

    added routing to student add form

--- src/pages/students/students-list/studentstopnav/studentstopnav.jsx
+++ src/pages/students/students-list/studentstopnav/studentstopnav.jsx
@@ -3,11 +3,13 @@ import "./studentstopnav.scss";
 
 
 const StudentListTopNav = () => {
+    const history = useHistory();
+
     return (
         <nav className="top-nav d-flex flex-column">
             <div className="top-nav__header d-flex align-items-center justify-content-between">
                 <h1 className="top-nav__header-title"> Students </h1>
-                <button className="btn-primary top-nav__header-button border-0 me-2">+ Create</button>
+                <button className="btn-primary top-nav__header-button border-0 me-2" onClick={() => history.push('/student/create')}>+ Create</button>
             </div>
             <div className="row top-nav__student-types d-flex justify-content-start align-items-center">
                 <p className="col top-nav__student-types__item">All</p>
