ab59142dbd408f004a0744da060aede5ee662f03
Reshmy Devaraju <reshmyd@gmail.com>
2021-10-04 16:54:53 +0100

    added controller to the star rating component

--- src/pages/students/students-projects-index/review/reviewform/reviewform.jsx
+++ src/pages/students/students-projects-index/review/reviewform/reviewform.jsx
@@ -5,6 +5,7 @@ import { useForm, Controller } from "react-hook-form";
 // import { FaStar } from 'react-icons/fa';
 // import Star from '../../../../../components/star/star';
 import StarRatingComponent from 'react-star-rating-component';
+import ReactStars from "react-rating-stars-component";
 
 const ReviewForm = ({ onSubmit }) => {
     const {control, handleSubmit,  reset } = useForm();
@@ -35,11 +36,12 @@ const ReviewForm = ({ onSubmit }) => {
             <div className="col-6">
                 <div className="col-12 mt-4">
                     <h6>Knowledge of Module</h6>
-                    
-                    <StarRatingComponent 
+                    <Controller control={control} name="module-rating" render={ ( {field : { value, onChange }} ) => ( <ReactStars
+                    value={value} onChange={onChange}
                     name="module-rating"
-                    starColor="#724BCB"  />
-                    <br/>
+                    activeColor="#724BCB" 
+                    size={17} /> )} />
+                    {/* <br/> */}
                       <label htmlFor="moduleKnowledge">Additional Notes:</label>
                      <Controller control={control} name="moduleKnowledge" render={ ( {field : { value, onChange }} ) => (<input
                       value={value} onChange={onChange}
@@ -52,9 +54,13 @@ const ReviewForm = ({ onSubmit }) => {
 
                 <div className="col-12 mt-3">
                     <h6>Quality of HTML/CSS</h6>
-                    <StarRatingComponent name="HtmlCss-rating"
-                    starColor="#724BCB"  />
-                    <br/>
+                    <Controller control={control} name="HtmlCss-rating" render={ ( {field : { value, onChange }} ) => ( <ReactStars
+                    value={value} onChange={onChange}
+                    name="HtmlCss-rating"
+                    activeColor="#724BCB"
+                    size={17}  /> )} />
+                    
+                    {/* <br/> */}
                     <label htmlFor="HTML-CSSKnowledge">Additional Notes:</label>
                     <Controller control={control} name="HTML-CSSKnowledge" render={ ( {field: { value, onChange }} ) => (<input
                       value={value} onChange={onChange}
@@ -68,9 +74,14 @@ const ReviewForm = ({ onSubmit }) => {
 
                 <div className="col-12 mt-3">
                     <h6>Quality of Javascript</h6>
-                    <StarRatingComponent name="js-rating"
-                    starColor="#724BCB"  />
-                    <br/>
+                    <Controller control={control} name="js-rating" render={ ( {field : { value, onChange }} ) => ( <ReactStars
+                    value={value} onChange={onChange}
+                    name="js-rating"
+                    activeColor="#724BCB" 
+                    size={17}  /> )} />
+                    
+                    
+                    {/* <br/> */}
                     <label htmlFor="jsKnowledge">Additional Notes:</label>
                     <Controller control={control} name="jsKnowledge" render={ ( {field: { value, onChange }} ) => (<input
                       value={value} onChange={onChange}
