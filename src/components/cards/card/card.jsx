cbecd07b04c91a99afd52bf33b77745c17f44c81
Noor Ncho <nrandin@gmail.com>
2021-10-04 17:08:01 +0100

    rendered additional notes from input

--- src/components/cards/card/card.jsx
+++ src/components/cards/card/card.jsx
@@ -2,7 +2,7 @@ import "./card.scss";
 
 const Card = ({project, btnText}) => {
   const card = (
-    <div className="card col-md-3" key={project.title}>
+    <div className="card col-3" key={project.title}>
       <img src={project.imgSrc} alt={project.title} />
       <h5 className="mt-3">{project.title}</h5>
       <p>{project.languagesUsed.join("/")}</p>
