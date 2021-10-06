43c59c6e30260dafde0b80717e0e6e29ed889b9e
Matthew Bickell <matthew@nology.io>
2021-10-06 14:57:15 +0100

    Removed use of useHistory and replaced it with the <Link> tag. Added styling to facilitate new <a> tag in the header

--- src/pages/students/students-list/studentstopnav/studentstopnav.jsx
+++ src/pages/students/students-list/studentstopnav/studentstopnav.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { useHistory } from 'react-router';
+import {  Link } from 'react-router-dom';
 import "./studentstopnav.scss";
 
 
 const StudentListTopNav = () => {
-    const history = useHistory();
-
     return (
         <nav className="top-nav d-flex flex-column">
             <div className="top-nav__header d-flex align-items-center justify-content-between">
                 <h1 className="top-nav__header-title"> Students </h1>
-                <button className="btn-primary top-nav__header-button border-0 me-2" onClick={() => history.push('/student/create')}>+ Create</button>
+                <Link to="/student/create" className="btn-primary top-nav__header-button border-0 me-2">+ Create</Link>
             </div>
             <div className="row top-nav__student-types d-flex justify-content-start align-items-center">
                 <p className="col top-nav__student-types__item">All</p>
