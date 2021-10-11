6f1c537a6eb45f26b78dc6e9eb942eda4bc07539
Andy Evans <andy@nology.io>
2021-10-11 14:51:22 +0100

    Merge remote-tracking branch 'origin/feature/09-

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Students from "./pages/students/students-list/students-list";
 import StudentDetails from './pages/students/student-details/student-details';
 import AddStudent from './pages/students/student-add/addstudent';
 import ProjectList from './pages/projects/projects-list/projects-list';
+import ProjectCreate from './pages/projects/projects-create/projects-create';
 
 function App() {
 
@@ -25,30 +26,13 @@ function App() {
         </div>
         <div className="col">
           <Switch>
-            
-
-            <Route path="/student/:id/project/:id/review">
-              <ProjectReview />
-            </Route>
-
-            <Route path="/student/:id/project/:id">
-              <SubmissionDetails />
-            </Route>
-            
-            <Route path="/student/:id">
-              <StudentDetails></StudentDetails>
-            </Route>
-
-            <Route path="/student/create">
-              <AddStudent></AddStudent>
-            </Route>
-       
-            <Route path={["/students", "/"]}>
-              <Students />
-            </Route>
-            
+                        
             <Route path="/projects">
               <ProjectList></ProjectList>
+            </Route>
+
+            <Route path="/project/create">
+              <ProjectCreate></ProjectCreate>
             </Route>
 
             <Route path="/projects/:studentId/:projectId">
@@ -59,7 +43,26 @@ function App() {
               <Settings />
             </Route>
 
-          
+            <Route path="/student/create">
+              <AddStudent></AddStudent>
+            </Route>
+
+            <Route path="/student/:id/project/:id/review">
+              <ProjectReview />
+            </Route>
+
+            <Route path="/student/:studentId/project/:projectId">
+              <SubmissionDetails />
+            </Route>
+            
+            {/* <Route path="/student/:studentId/project/:projectId">
+              <StudentDetails></StudentDetails>
+            </Route> */}
+       
+            <Route path={["/students", "/"]}>
+              <Students />
+            </Route>
+
           </Switch>
         </div>
       </Router>
--- src/pages/projects/projects-list/projects-top-nav/projects-top-nav.jsx
+++ src/pages/projects/projects-list/projects-top-nav/projects-top-nav.jsx
@@ -9,7 +9,7 @@ const StudentListTopNav = (props) => {
         <nav className="top-nav d-flex flex-column justify-content-between">
             <div className="top-nav__header d-flex align-items-center justify-content-between">
                 <h1 className="top-nav__header-title"> Projects </h1>
-                <Link to="/projects-create" className="btn-primary top-nav__header-button border-0 me-2">+ Create</Link>
+                <Link to="/project/create" className="btn-primary top-nav__header-button border-0 me-2">+ Create</Link>
             </div>
             <div className="top-nav__language-types d-flex justify-content-start align-items-center">
                 <div className={languageFilter === "All" ? "top-nav__language-types__all selectedFilterClass" : "top-nav__language-types__all"} onClick={filterDataByAll}>All</div>
