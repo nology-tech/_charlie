9f0229c27018852432de4594d689cca19108ce5b
Wei-Tong Tang <weitongblue@gmail.com>
2021-10-11 16:00:54 +0100

    some new skeletons

---
+++ src/components/pageContainer/pageContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const pageContainer = () => {
+    return (
+        <div className ="main">
+            <div className="projects__white-space"></div>
+                <div className="main__content d-flex flex-column align-items-center p-0 ">
+                    {/* navbar */}
+                    {/* page content */}
+                </div>
+            <div className="projects__white-space"></div>
+        </div>
+    )
+}
+
+export default pageContainer
