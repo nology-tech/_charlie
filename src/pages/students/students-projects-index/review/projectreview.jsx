83677cdfadc5f3bc9be0c13aba7862c0f4714ab7
Salvador Martinez <cartoonrocket9@gmail.com>
2021-10-11 18:07:30 +0200

    fix

--- src/pages/students/students-projects-index/review/projectreview.jsx
+++ src/pages/students/students-projects-index/review/projectreview.jsx
@@ -1,17 +1,23 @@
 import ProjectReviewTopNav from "./projectreviewtopnav/projectreviewtopnav";
 import ProjectReviewForm from "./projectreviewform/projectreviewform";
 
+import './projectreview.scss';
+
 const ProjectReview = () => {
   const onSubmit = (data) => {
     console.log(data);
     alert(JSON.stringify(data));
-};
+  };
   return (
-    <div>
-      <ProjectReviewTopNav onSubmit={onSubmit} />
-      <ProjectReviewForm onSubmit={onSubmit} />
+    <div className="main">
+      <div className="projects__white-space"></div>
+      <div className="main__content d-flex flex-column p-0 ">
+        <ProjectReviewTopNav onSubmit={onSubmit} />
+        <ProjectReviewForm onSubmit={onSubmit} />
+      </div>
+      <div className="projects__white-space"></div>
     </div>
-  )
-}
+  );
+};
 
 export default ProjectReview;
