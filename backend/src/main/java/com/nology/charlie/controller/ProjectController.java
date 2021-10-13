923ba2446a1e2eb37bf58742710abd25fa456c2b
saagarshah99 <saagarshahs@hotmail.com>
2021-10-13 14:38:12 +0100

    Started exception handling

--- backend/src/main/java/com/nology/charlie/controller/ProjectController.java
+++ backend/src/main/java/com/nology/charlie/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.nology.charlie.controller;
 
 import com.nology.charlie.entity.Message;
 import com.nology.charlie.entity.Project;
+import com.nology.charlie.exceptions.ResourceNotFoundException;
 import com.nology.charlie.repository.IProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,26 +10,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("projects")
 public class ProjectController {
     @Autowired
     private IProjectRepository repo;
 
-    @GetMapping("projects")
+    @GetMapping("/")
     public ResponseEntity<List> getProjects() {
         return ResponseEntity.status(HttpStatus.OK).body(this.repo.findAll());
     }
 
-    @GetMapping("/projects/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Object> getAProject(@PathVariable int id) {
-       return ResponseEntity.status(HttpStatus.OK).body(this.repo.findById(id));
+        if(this.repo.findById(id) == null)
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new ResourceNotFoundException("that was bad"));
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.repo.findById(id));
     }
 
-    @PostMapping("/projects")
-    public ResponseEntity<Message> createAProject(@RequestBody Project newProject) {
+    @PostMapping("/")
+    public Message createAProject(@RequestBody Project newProject) {
         this.repo.save(newProject);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new Message("Successfully added a new project"));
+        return new Message("Successfully added a new project");
     }
 
 
