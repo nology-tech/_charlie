ac985ee7082677c09e8862913c099b55c335f274
saagarshah99 <saagarshahs@hotmail.com>
2021-10-13 15:26:32 +0100

    checking if project id exists

--- backend/src/main/java/com/nology/charlie/controller/ProjectController.java
+++ backend/src/main/java/com/nology/charlie/controller/ProjectController.java
@@ -2,7 +2,6 @@ package com.nology.charlie.controller;
 
 import com.nology.charlie.entity.Message;
 import com.nology.charlie.entity.Project;
-import com.nology.charlie.exceptions.ResourceNotFoundException;
 import com.nology.charlie.repository.IProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,43 +11,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("projects")
+@RequestMapping("/projects/")
 public class ProjectController {
     @Autowired
     private IProjectRepository repo;
 
-    @GetMapping("/")
+    @GetMapping("")
     public ResponseEntity<List> getProjects() {
         return ResponseEntity.status(HttpStatus.OK).body(this.repo.findAll());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getAProject(@PathVariable int id) {
-        if(this.repo.findById(id) == null)
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body(new ResourceNotFoundException("Could not find project with id: " + id));
+    @GetMapping("{id}")
+    public Object getAProject(@PathVariable int id) {
+        if(this.repo.existsById(id))
+            return this.repo.findById(id);
 
-        return ResponseEntity.status(HttpStatus.OK).body(this.repo.findById(id));
+        return new Message("Could not find project with id: " + id);
     }
 
-    @PostMapping("/")
+    @PostMapping("")
     public Message createAProject(@RequestBody Project newProject) {
         this.repo.save(newProject);
         return new Message("Successfully added a new project");
     }
 
-    @PutMapping("/{id}")
-    public Project updateProjectById(@PathVariable int id, @RequestBody Project updatedProject) {
-        this.repo.deleteById(id);
-        this.repo.save(updatedProject);
-        return updatedProject;
+    @PutMapping("{id}")
+    public Object updateProjectById(@PathVariable int id, @RequestBody Project updatedProject) {
+        if(this.repo.existsById(id)) {
+            this.repo.deleteById(id);
+            this.repo.save(updatedProject);
+            return updatedProject;
+        }
+        return new Message("Could not find project with id: " + id);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     public Message deleteProjectById(@PathVariable int id) {
-        this.repo.deleteById(id);
-        return new Message("Successfully deleted project with id: " + id);
+        if(this.repo.existsById(id)) {
+            this.repo.deleteById(id);
+            return new Message("Successfully deleted project with id: " + id);
+        }
+        return new Message("Could not find project with id: " + id);
     }
-
 }
