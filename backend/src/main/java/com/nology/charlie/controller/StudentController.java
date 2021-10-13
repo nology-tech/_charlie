4b673932f3ffb1ac2b56f45cd52151515ced1194
Noor Ncho <nrandin@gmail.com>
2021-10-13 13:05:37 +0100

    Added endpoints

--- backend/src/main/java/com/nology/charlie/controller/StudentController.java
+++ backend/src/main/java/com/nology/charlie/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.nology.charlie.controller;
 
 import com.nology.charlie.entity.Message;
 import com.nology.charlie.entity.Student;
+import com.nology.charlie.exception.ResourceNotFoundException;
 import com.nology.charlie.repository.IStudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,23 +14,52 @@ import java.util.List;
 public class StudentController {
 
     @Autowired
-    IStudentRepository repository;
+    IStudentRepository studentRepository;
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        return this.repository.findAll();
+        return this.studentRepository.findAll();
+    }
+
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getOneStudent(@PathVariable int id) throws ResourceNotFoundException {
+        return ResponseEntity.status(HttpStatus.OK).body(studentRepository.getById(id));
     }
 
     @DeleteMapping("/students/{id}")
     public ResponseEntity<Message> deleteStudent(@PathVariable int id) {
-        repository.deleteById(id);
+        studentRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body(new Message("Successfully deleted a student"));
     }
     @PostMapping("/students")
     public List <Student> addStudent (@RequestBody Student newStudent) {
-        this.repository.save(newStudent);
-        return this.repository.findAll();
+        this.studentRepository.save(newStudent);
+        return this.studentRepository.findAll();
     }
 
+    @PutMapping("/students/{id}")
+    public ResponseEntity <Message> updatedStudent(@PathVariable int id, @RequestBody Student newStudent) throws ResourceNotFoundException{
+        /*// Get recipe from database
+        Student student = repository.getById(id);
 
+        // Edit recipe
+        existingRecipe.setDuration(newRecipe.getDuration());
+        existingRecipe.setServings(newRecipe.getServings());
+        existingRecipe.setTitle(newRecipe.getTitle());
+
+        // Store recipe back in database
+        repository.save(newStudent);
+
+        // Send back to client
+        return ResponseEntity.status(HttpStatus.OK).body(new Message("Successfully updated Student"));*/
+        Student student = updateStudentById(id, newStudent);
+        return ResponseEntity.status(HttpStatus.OK).body(new Message("Successfully updated Student"));
+    }
+
+    public Student updateStudentById(int id, Student newStudent) {
+        Student existingStudent = studentRepository.getById(id);
+        studentRepository.findAll().set(id, newStudent);
+        Student foundStudent = studentRepository.getById(id);
+        return foundStudent;
+    }
 }
