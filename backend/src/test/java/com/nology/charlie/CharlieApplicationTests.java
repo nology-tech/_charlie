972900104c06bc80d76445632e7e567ef4ae0a13
Andy Evans <andy@nology.io>
2021-10-13 11:13:38 +0100

    repository restructure

---
+++ backend/src/test/java/com/nology/charlie/CharlieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nology.charlie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharlieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
