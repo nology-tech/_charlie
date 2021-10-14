0377610a4a63aab58a2ef7924c3dec8de084d231
Noor Ncho <nrandin@gmail.com>
2021-10-14 17:50:50 +0100

    added more styling

--- frontend/src/pages/students/student-overview/studentoverview.jsx
+++ frontend/src/pages/students/student-overview/studentoverview.jsx
@@ -2,6 +2,8 @@ import Cards from 'components/cards/cards';
 import React, { useState, useEffect } from 'react'
 import { FaGithub } from 'react-icons/fa';
 
+import thumbnail from "../../../assets/images/thumbnail-placeholder.png";
+
 const StudentOverview = () => {
     const [studentsData, setStudentsData] = useState({});
 
@@ -23,12 +25,16 @@ const StudentOverview = () => {
             <div className="overview row mt-4 mx-3">
                 <h3 className="overview__title">Overview</h3>
                 <div className="overview__card card d-flex flex-column col-3 ">
+                    <img src={thumbnail} alt="Student Thumbnail" className="overiew__image" />
                     <h4>Name {studentsData.studentName}</h4>
                     <p>{studentsData.enrolloedOn} Student</p>                
                 </div>
                 <div className="overview__github card d-flex flex-column offset-1 col-8">
                     <h4>GitHub</h4>
                     GitHub: {studentsData.githubAccount}
+                    <div className="overview__github-info">
+                        
+                    </div>
                     <button className="btn-dark btn-project">
                         <FaGithub />&nbsp;View Repo
                     </button>
