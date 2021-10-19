e276c061b10f32c5eccb0c7ac25fe8ebb7af2ae9
Wei-Tong Tang <weitongblue@gmail.com>
2021-10-19 16:04:44 +0100

    styled student form

--- frontend/src/pages/students/student-add/addstudent.jsx
+++ frontend/src/pages/students/student-add/addstudent.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import AddStudentTopNav from './addstudenttopnav/addstudenttopnav';
 import StudentForm from './studentform/studentform';
 import "./addstudent.scss";
+import PageHeader from 'components/page-header/page-header';
 
 const AddStudent = () => {
     return (
-        <div className="add-student">
-            <AddStudentTopNav />
-            <StudentForm />
+        <div className ="main m-0 d-flex justify-content-between">
+            <div className="main__white-space"></div>
+                <div className="main__content d-flex flex-column align-items-centerp-0">
+                    <PageHeader
+                    title = {"Add Student"}
+                    buttonPath={"/students"}
+                    buttonText={"Go Back"}
+                    buttonStyle={"btn-back top-nav__header-button me-2"}/>
+                    <StudentForm />
+                </div>
+            <div className="projects__white-space"></div>
         </div>
     )
 }
--- frontend/src/pages/students/student-add/studentform/studentform.jsx
+++ frontend/src/pages/students/student-add/studentform/studentform.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 //import studentData from '../../data/studentForm';
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router";
-
+import Thumb from "assets/images/project-thumbnail.png"
 const StudentForm = () => {
     const {register, handleSubmit, formState: { errors }, reset } = useForm();
     const history = useHistory();
@@ -55,155 +55,153 @@ const StudentForm = () => {
     };
 
     return (
-        <div className="row mt-4 my-4 px-4 mx-auto offset-1 form__container">
-        <form className="row" onSubmit={handleSubmit(onSubmit)}>
-            <div className="col-6">
-            <div className="col-12 mt-4">
-                <label htmlFor="firstName">First Name</label>
-                <input
-                {...register("firstName", {
-                    required: true,
-                    pattern: /^[A-Za-z]+$/i,
-                })}
-                name="firstName"
-                className="form-control form__input  my-2"
-                type="text"
-                id="firstName"
-                data-testid="firstName"
-                />
-                {errors.firstName && <div className="text-danger">*Required</div>}
-            </div>
+        <div className="row mt-4 w-100 my-4 px-4 mx-auto offset-1 form__container">
+            <form className="row" onSubmit={handleSubmit(onSubmit)}>
+                <div className="columns col-6">
+                    <div className="col-12 mt-4">
+                        <label htmlFor="firstName" className="addStudentLabel fw-bold">First Name</label>
+                        <input
+                        {...register("firstName", {
+                            required: true,
+                            pattern: /^[A-Za-z]+$/i,
+                        })}
+                        name="firstName"
+                        className="form-control form__input  my-2"
+                        type="text"
+                        id="firstName"
+                        data-testid="firstName"
+                        />
+                        {errors.firstName && <div className="text-danger">*Required</div>}
+                    </div>
 
-            <div className="col-12 mt-3">
-                <label htmlFor="lastName">Last Name</label>
-                <input
-                {...register("lastName", {
-                    required: true,
-                    pattern: /^[A-Za-z]+$/i,
-                })}
-                name="lastName"
-                className="form-control form__input my-2"
-                type="text"
-                id="lastName"
-                data-testid="lastName"
-                />
-                {errors.lastName && <p className="text-danger">*Required</p>}
-            </div>
+                    <div className="col-12 mt-3">
+                        <label htmlFor="lastName" className="addStudentLabel fw-bold">Last Name</label>
+                        <input
+                        {...register("lastName", {
+                            required: true,
+                            pattern: /^[A-Za-z]+$/i,
+                        })}
+                        name="lastName"
+                        className="form-control form__input my-2"
+                        type="text"
+                        id="lastName"
+                        data-testid="lastName"
+                        />
+                        {errors.lastName && <p className="text-danger">*Required</p>}
+                    </div>
 
-            <div className="col-12 mt-3">
-                <label htmlFor="email">Email</label>
-                <input
-                {...register("email", {
-                    required: true
-                })}
-                name="email"
-                className="form-control form__input my-2"
-                type="email"
-                id="email"
-                />
-                {errors.email && <p className="text-danger">*Required</p>}
-            </div>
+                    <div className="col-12 mt-3">
+                        <label htmlFor="email" className="addStudentLabel fw-bold">Email</label>
+                        <input
+                        {...register("email", {
+                            required: true
+                        })}
+                        name="email"
+                        className="form-control form__input my-2"
+                        type="email"
+                        id="email"
+                        />
+                        {errors.email && <p className="text-danger">*Required</p>}
+                    </div>
 
-            <div className="col-12 mt-3">
-                <label htmlFor="">Enrolled on</label>
-                <select
-                {...register("enrolledOn", { required: true })}
-                name="enrolledOn"
-                className="form-select form-control form__input  my-2"
-                id="enrolledOn"
-                data-testid="enrolledOn"
-                >
-                <option selected="true" value="default">
-                    Select one of the following
-                </option>
-                <option value="ibiza">Ibiza</option>
-                <option value="hawaii">Hawaii</option>
-                <option value="jersey">Jersey</option>
-                </select>
-            </div> 
+                    <div className="col-12 mt-3">
+                        <label htmlFor="" className="addStudentLabel fw-bold">Enrolled on</label>
+                        <select
+                        {...register("enrolledOn", { required: true })}
+                        name="enrolledOn"
+                        className="form-select form-control form__input  my-2"
+                        id="enrolledOn"
+                        data-testid="enrolledOn"
+                        >
+                        <option selected="true" value="default">
+                            Select one of the following
+                        </option>
+                        <option value="ibiza">Ibiza</option>
+                        <option value="hawaii">Hawaii</option>
+                        <option value="jersey">Jersey</option>
+                        </select>
+                    </div> 
 
-            <div className="col-12 mt-3">
-                <label htmlFor="">Enrolled Type</label>
-                <select
-                {...register("enrolledType", { required: true })}
-                name="enrolledType"
-                className="form-select form-control form__input  my-2"
-                id="enrolledType"
-                data-testid="enrolledType"
-                >
-                <option selected="true" value="default">
-                    Select one of the following
-                </option>
-                <option value="Full-Time">Full Time</option>
-                <option value="Corporate">Corporate</option>
-                <option value="Self-Paced">Self Paced</option>
-                </select>
-            </div> 
-            
-            
+                    <div className="col-12 mt-3">
+                        <label htmlFor="" className="addStudentLabel fw-bold">Enrolled Type</label>
+                        <select
+                        {...register("enrolledType", { required: true })}
+                        name="enrolledType"
+                        className="form-select form-control form__input  my-2"
+                        id="enrolledType"
+                        data-testid="enrolledType"
+                        >
+                        <option selected="true" value="default">
+                            Select one of the following
+                        </option>
+                        <option value="Full-Time">Full Time</option>
+                        <option value="Corporate">Corporate</option>
+                        <option value="Self-Paced">Self Paced</option>
+                        </select>
+                    </div> 
+                    
+                    <div className="col-12 mt-3">
+                        <label htmlFor="githubAccount" className="addStudentLabel fw-bold">Github Account</label>
+                        <input
+                        {...register("githubAccount", { required: true })}
+                        name="githubAccount"
+                        className="form-control form__input my-2"
+                        type="text"
+                        id="githubAccount"
+                        data-testid="githubAccount"
+                        />
+                        {errors.githubAccount && <p className="text-danger">*Required</p>}
+                    </div>
 
-            <div className="col-12 mt-3">
-                <label htmlFor="githubAccount">Github Account</label>
-                <input
-                {...register("githubAccount", { required: true })}
-                name="githubAccount"
-                className="form-control form__input my-2"
-                type="text"
-                id="githubAccount"
-                data-testid="githubAccount"
-                />
-                {errors.githubAccount && <p className="text-danger">*Required</p>}
-            </div>
-
-            <div className="col-12 mt-3">
-                <label htmlFor="">Portfolio Link</label>
-                <input
-                    {...register("portfolioLink", { required: true })}
-                    name="portfolioLink"
-                    className="form-control form__input my-2"
-                    type="text"
-                    id="portfolioLink"
-                    data-testid="portfolioLink"
-                />
-                {errors.portfolioLink && <p className="text-danger">*Required</p>}
-            </div>
-            </div>
-            <div className="col-6">
-            <div className="col-12 mt-4">
-                <label htmlFor="studentThumb">Student Thumbnail</label>
-                <input {...register("studentThumb")}
-                type="file"
-                onChange={readURL}
-                name="studentThumb"
-                id="studentThumb"
-                data-testid="studentThumb"
-                hidden
-                />
-                <img id="studentImage" src={src} alt={alt} className="student-thumbnail my-2" />
-                <label htmlFor="studentThumb" className="btn upload-btn mt-4">
-                Upload
-                </label>
-            </div>
-            <div className="col-12 mt-4">
-                <label className="my-3">CV Upload</label>
-                <div>
-                    <label htmlFor="cvUpload" className="btn upload-btn mt-2">Upload</label>
-                    <input {...register("cvUpload")}
-                    type="file"
-                    className="btn btn-upload"
-                    name="cvUpload"
-                    id="cvUpload"
-                    data-testid="cvUpload"
-                    hidden
-                    />
+                    <div className="col-12 mt-3">
+                        <label htmlFor="" className="addStudentLabel fw-bold">Portfolio Link</label>
+                        <input
+                            {...register("portfolioLink", { required: true })}
+                            name="portfolioLink"
+                            className="form-control form__input my-2"
+                            type="text"
+                            id="portfolioLink"
+                            data-testid="portfolioLink"
+                        />
+                        {errors.portfolioLink && <p className="text-danger">*Required</p>}
+                    </div>
                 </div>
-            </div>
-            <div className="col-12 mt-5">
-                <input type="reset" className="btn btn-secondary my-5 mx-2 form__button" value="Cancel" onClick={handleClick} />
-                <input type="submit" className="btn btn-primary my-5 form__button" value="Save" />
-            </div>
-            </div>
-        </form>
+                <div className="columns col-6">
+                    <div className="col-12 mt-4">
+                        <label htmlFor="studentThumb" className="addStudentLabel  gx-0 row fw-bold">Student Thumbnail</label>
+                        <input {...register("studentThumb")}
+                        type="file"
+                        onChange={readURL}
+                        name="studentThumb"
+                        id="studentThumb"
+                        data-testid="studentThumb"
+                        hidden
+                        />
+                        <img id="studentImage" src={Thumb} alt={Thumb} className="student-thumbnail  gx-0 row my-2" />
+                        <label htmlFor="studentThumb" className="btn upload-btn mt-4">
+                        Upload
+                        </label>
+                    </div>
+                    <div className="col-12 mt-4">
+                        <label className=" addStudentLabel my-3 row gx-0 fw-bold">CV Upload</label>
+                        <div>
+                            <label htmlFor="cvUpload" className="btn upload-btn mt-2">Upload</label>
+                            <input {...register("cvUpload")}
+                            type="file"
+                            className="btn btn-upload"
+                            name="cvUpload"
+                            id="cvUpload"
+                            data-testid="cvUpload"
+                            hidden
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 mt-5 d-flex align-items-end">
+                        <input type="reset" className="form__button-cancel" value="Cancel" onClick={handleClick} />
+                        <input type="submit" className="form__button-save" value="Save" />
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
